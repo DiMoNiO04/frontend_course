1489a0c2b81a92cdc82502a3558250d5c4d6d15f
DiMoNiO04 <dima.razumov.940@mail.ru>
2024-12-20 21:23:53 +0300

    feat: add config for router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,8 @@
-import { Route, Routes } from "react-router-dom";
 import './styles/index.scss';
 import { Link } from "react-router-dom";
-import { Suspense } from "react";
 import { useTheme } from "./providers/ThemeProvider";
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
 import { classNames } from "shared/lib/classNames";
+import { AppRouter } from './providers/router';
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -17,12 +14,7 @@ const App = () => {
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
       
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={'/about'} element={<AboutPage />} />
-          <Route path={'/'} element={<MainPage />} />
-        </Routes> 
-      </Suspense>
+      <AppRouter />
     </div>
   )
 }
