7c70f288a066ed65e2adaa27462b8f7d73fd55c1
DiMoNiO04 <dima.razumov.940@mail.ru>
2025-01-06 14:00:38 +0300

    feat: add hot reload plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -15,6 +15,7 @@ export function buildPlugins({paths}: BuildOptions): webpack.WebpackPluginInstan
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(true)
-    })
+    }),
+    new webpack.HotModuleReplacementPlugin()
   ]
 }
