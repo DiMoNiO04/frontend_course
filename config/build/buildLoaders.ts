13de991949a06991a454332c5cb15143b9a7413e
DiMoNiO04 <dima.razumov.940@mail.ru>
2025-01-07 11:55:09 +0300

    feat: add babel and extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,11 +2,31 @@ import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import webpack from "webpack";
 import { BuildOptions } from "./types/config";
 
-export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
 
   const svgLoader = {
     test: /\.svg$/,
     use: ['@svgr/webpack'],
+  }
+
+  const babelLoader = {
+    test: /\.(js|jsx|ts|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ['@babel/preset-env'],
+        plugins: [
+          [
+            "i18next-extract", 
+            {
+              locales: ['ru', 'en'],
+              keyAsDefaultValue: true
+            }
+          ],
+        ]
+      }
+    }
   }
 
   const fileLoader = {
@@ -27,7 +47,7 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
         options: {
           modules: {
             auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-            localIdentName: isDev 
+            localIdentName: isDev
               ? "[path][name]__[local]--[hash:base64:5]"
               : "[hash:base64:8]",
           }
@@ -46,7 +66,8 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
   return [
     fileLoader,
     svgLoader,
+    babelLoader,
     typescriptLoader,
-    cssLoader,
+    cssLoader
   ]
 }
