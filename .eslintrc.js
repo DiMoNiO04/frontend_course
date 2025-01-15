2176d29ec50737c27fad83b98fa5cef031da993b
DiMoNiO04 <dima.razumov.940@mail.ru>
2025-01-15 22:53:57 +0300

    feat: add loader and not found page

--- .eslintrc.js
+++ .eslintrc.js
@@ -40,6 +40,7 @@ module.exports = {
     'no-underscore-dangle': 'off',
     'linebreak-style': 0,
     'i18next/no-literal-string': ['error', { markupOnly: true }],
+    'max-len': ['error', { "ignoreComments": true }]
   },
   globals: {
     __IS_DEV__: true,
